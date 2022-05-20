f6a82e064fb9e01275a29b2231fa4de9a2d68f6d
Nylzen <tugoresn@gmail.com>
2022-05-19 21:30:33 -0300

    Javascript 1

---
+++ Javascript/clase 1/main.js
@@ -0,0 +1,122 @@
+let numeroA = 157;
+let numeroB = 238;
+
+let resultadoNumero = numeroA + numeroB;
+
+// console.log(resultadoNumero);
+
+let textoA = "Harry";
+let textoB = "Potter";
+let resultadoTexto = textoA + " " + textoB;
+
+// console.log(
+//   "El nombre de la pelicula es: " +
+//     resultadoTexto +
+//     " " +
+//     "El resultado de la suma es: " +
+//     resultadoNumero
+// );
+
+// Tipos de datos
+/*
+- Number = 1/35/49 
+- Strings = 'Hola' "Hola" `hola`
+- Booleanos = true, false
+- undefined = algo indefinido
+- null = vacio
+- funciones
+- objetos
+*/
+
+// let no podemos volver a declarla con el mismo nombre, pero si podemos reasignarle un nuevo valor
+// let nombre = "Nelson";
+// nombre = "Hola";
+
+// var podemos redeclararla y se va a pisar el valor
+// var nombre = "Nelson"
+//  var nombre = " Pepito"
+// * Esto les devuelve pepito
+
+// const nombre = "Nelson";
+// let age = 26;
+// age = 27;
+
+// console.log(age);
+
+// * Operadores
+/*
+ + = suma
+ - = resta
+ * = multiplicacion
+ / = division
+ ** = calcular la base a la potencia de exponente
+ % = Modulo, nos da el resto
+ ++ = incrementador, suma 1
+ -- = decrementador, restar 1
+*/
+
+let numeritoUno = 10;
+let numeritoDos = 5;
+
+// console.log(numeritoUno + numeritoDos);
+// console.log(numeritoUno * numeritoDos);
+// console.log(numeritoUno % numeritoDos);
+// console.log(numeritoUno / numeritoDos);
+
+// Sumar Strings
+let saludo = "Hola, mi nombre es ";
+let nombre = "Pepito";
+
+// console.log(saludo + nombre);
+// console.log(`${saludo} ${nombre}`);
+
+// Traspasamos el console anterior a bakticks
+// console.log(
+//   "El nombre de la pelicula es: " +
+//     resultadoTexto +
+//     " " +
+//     "El resultado de la suma es: " +
+//     resultadoNumero
+// );
+
+// console.log(
+//   `El nombre de la pelicula es: ${resultadoTexto}. El resultado de la suma es: ${resultadoNumero}`
+// );
+
+//* Operadores de Comparacion
+/*
+== = comprar los valores
+=== = comparar los valores y el tipo de dato
+!= = diferentes valores
+!== = difrente valor y diferente tipo de dato
+> mayor que
+< menor que
+>= mayor o igual 
+<= menor o igual
+
+&& = y - and
+|| = o - or
+! = negar algo
+*/
+
+// console.log(5 == 5);
+// console.log(5 == "5");
+// console.log(5 === 5); // porque compara el valor y el tipo de dato
+
+// Como sabemos que tipo de datos estamos manejando?
+// console.log(typeof 5);
+// console.log(typeof "5");
+// console.log(typeof 5.3);
+// console.log(typeof true);
+
+// Prompt
+// let dato = prompt("Como te llamas?");
+
+// console.log(dato);
+
+// alert(`Tu nombre es ${dato}`);
+
+let numerito1 = 1;
+
+let _numero = "hola";
+let Numero = 1;
